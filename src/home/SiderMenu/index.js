9b2436d0caa4d097b8798aaef2942be210c2301c
zhangZhiHao <137596665@qq.com>
2019-02-15 14:49:46 +0800

    侧边栏增加动画效果

--- src/home/SiderMenu/index.js
+++ src/home/SiderMenu/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import style from './style/index.module.less'
 import {Switch} from 'antd-mobile'
+import {inject,observer} from 'mobx-react'
 
 
-
+@inject('appStore') @observer
 class Sider extends React.Component{
     state = {
         checked:false,
@@ -12,6 +13,7 @@ class Sider extends React.Component{
 
     }
     render(){
+        const { isExpandSider } = this.props.appStore
         return (
             <div className={style.container}>
                 <div className={style.header}>
@@ -22,8 +24,8 @@ class Sider extends React.Component{
                 </div>
                 <div className={style.contenr}>
                     <div className={style.section}>
-                        <div className={style.title}>功能</div>
-                        <ul>
+                        <div className={`${style.title} ${style['fade-init']} ${isExpandSider ? style['fade'] : ''}`}>功能</div>
+                        <ul className={`${style['side-up-init']} ${isExpandSider ? style['side-up'] : ''}`}>
                             <li>
                                 <div className={'iconfont icon-ziyuanldpi'} style={{fontSize:16}}/>
                                 <div>均衡器</div>
@@ -50,15 +52,16 @@ class Sider extends React.Component{
                                 <div><Switch checked={this.state.checked} onChange={()=>this.setState({checked:!this.state.checked})}/></div>
                             </li>
                         </ul>
+                        <div className={`${style.divider} ${style['fade-init']} ${isExpandSider ? style['fade'] : ''}`}/>
                     </div>
                     <div className={style.section}>
-                        <div className={style.title}>通用</div>
-                        <ul>
-                            <li>
+                        <div className={`${style.title} ${style['fade-init']} ${isExpandSider ? style['fade'] : ''}`}>通用</div>
+                        <ul className={`${style['side-up-init']} ${isExpandSider ? style['side-up'] : ''}`}>
+                            <li style={{transitionDelay: '.56s'}}>
                                 <div className={'iconfont icon-guanyuwomen'} style={{fontSize:18}}/>
                                 <div>关于</div>
                             </li>
-                            <li>
+                            <li style={{transitionDelay: '.64s'}}>
                                 <div className={'iconfont icon-fankui'} style={{fontSize:18}}/>
                                 <div>帮助与反馈</div>
                             </li>
