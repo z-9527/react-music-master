97d0b7b4f3315d6c7507cd644982e7743d0837d4
zhangZhiHao <137596665@qq.com>
2019-02-16 09:27:53 +0800

    完善HeaderInfo组件

--- src/components/HeaderInfo/index.js
+++ src/components/HeaderInfo/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import style from './style/index.module.less'
 import {formatNumber} from '@/utils/util'
+import dayjs from 'dayjs'
 
 const HeaderInfo = (props)=> {
     const info = props.info
+    console.log(111,info)
     return (
         <div className={style.container}>
             <img src={info.coverImgUrl || ''} alt="" className={style['bg-img']}/>
@@ -12,8 +14,13 @@ const HeaderInfo = (props)=> {
                     <img src={info.coverImgUrl} alt=""/>
                     <p><span className={'iconfont icon-erji1'} style={{fontSize:12}}/> {formatNumber(info.playCount)}</p>
                 </div>
-                <div>
-
+                <div className={style.right}>
+                    <div className={style.name}>{info.name}</div>
+                    <div className={style.author}>
+                        <img src={info.creator && info.creator.avatarUrl} alt=""/>
+                        <span>{info.creator && info.creator.nickname}</span>
+                    </div>
+                    <div>{dayjs(info.updateTime).format('YYYY-MM-DD')} 更新</div>
                 </div>
             </div>
         </div>
